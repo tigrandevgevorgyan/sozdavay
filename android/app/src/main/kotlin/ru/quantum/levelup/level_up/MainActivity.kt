f0c1fce2deccdfcee89fd6141f78c48278c55760
elis-li <petrushina.2014@mail.ru>
2025-07-12 12:02:32 +0300

    timer fixes

--- android/app/src/main/kotlin/ru/quantum/levelup/level_up/MainActivity.kt
+++ android/app/src/main/kotlin/ru/quantum/levelup/level_up/MainActivity.kt
@@ -2,16 +2,38 @@ package ru.quantum.levelup.level_up
 import android.os.Bundle
 import android.content.Context
 import android.media.AudioManager
+import android.media.AudioFocusRequest
+import android.media.AudioAttributes
+import android.os.Build
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val focusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                .setAudioAttributes(
+                    AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .build()
+                )
+                .setOnAudioFocusChangeListener {}
+                .build()
+
+            audioManager.abandonAudioFocusRequest(focusRequest)
+        } else {
+            @Suppress("DEPRECATION")
+            audioManager.abandonAudioFocus(null)
+        }
+
         audioManager.mode = AudioManager.MODE_NORMAL
     }
 }
+
 
 
